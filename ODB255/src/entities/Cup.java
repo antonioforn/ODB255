51a07f0eee708761431dd938102e72fb6a0a5872
faster <faster@faster-PC>
2014-06-18 18:36:11 -0400

    declarando entidades

--- ODB255/src/entities/Cup.java
+++ ODB255/src/entities/Cup.java
@@ -1,12 +1,26 @@
 
 package entities;
 
+import java.util.Set;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 
 @Entity
 public class Cup {
-    @Id @GeneratedValue  private long id;
+    @Id @Column (name= "championship") 
+    private String championship;
+    
+    @ManyToMany
+    @JoinTable(name="CUP_TEAM",
+        joinColumns= 
+                @JoinColumn(name="CupName", referencedColumnName="championship"),
+        inverseJoinColumns=
+                @JoinColumn(name="TeamName", referencedColumnName="teamName")            
+    ) private Set<Team> teams;
     
 }
---
+++ ODB255/src/entities/Team.java
@@ -0,0 +1,11 @@
+
+
+package entities;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Team {
+    @Id String teamName;
+    
+}
